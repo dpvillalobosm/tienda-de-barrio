a95ee5d49a18092093f8370d188b41007f27d573
Camilo Andrés Díaz Serrano <camilodiaz369@gmail.com>
2021-12-10 12:22:45 -0500

    Delete legacy code

--- app/clientes/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-# TODO: Poner aquí la lógica del servicio de consulta de clientes almacenados en la BD de mongo
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
